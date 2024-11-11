b338a23e38bd076995d1f0c8b68bbeef308fe52e
thomaswknight <51743941+thomaswknight@users.noreply.github.com>
2024-11-11 09:56:17 +0000

    add determineOutcome function

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 const RPS = ["rock", "paper", "scissors"]
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3);
-    console.log(computerChoice);
-    return RPS[computerChoice];
+    const choice = Math.floor(Math.random() * 3);
+    console.log(choice);
+    document.getElementById("computerchoice").innerHTML =
+    "Computer chose " + RPS[choice]
+    return RPS[choice];
+
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    const humanChoice = prompt("Rock, Paper, or Scissors?")
-    console.log(humanChoice)
-    console.log(humanChoice in RPS)
-    for (i in RPS) { console.log(i)}
-    console.log(RPS)
-
-    if (RPS.includes(humanChoice.toLowerCase())) {
+    let choice = prompt("Rock, Paper, or Scissors?")
+    if (RPS.includes(choice.toLowerCase())) {
         document.getElementById("humanchoice").innerHTML = 
-        "you chose " + humanChoice;
+        "You chose " + choice;
+        return choice.toLowerCase();
     } else {
         document.getElementById("humanchoice").innerHTML = 
         "You must choose rock, paper, or scissors!";
+        getHumanChoice();
     }
 }
 
-getHumanChoice();
+function determineOutcome(choiceA, choiceB) {
+    let A = RPS.indexOf(choiceA);
+    let B = RPS.indexOf(choiceB);
+    console.log(A,B);
+    console.log(A == B);
+    console.log(A == (B + 1) % 3);
+    console.log(B == (A + 1) % 3);
+    if (A == B) {
+        return "Draw";
+    }
+    else if (A = (B + 1) % 3) {
+        return choiceA;
+    }
+    else if (B == (A + 1) % 3) {
+        return choiceB; 
+    }
+}
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    console.log(humanChoice);
+    console.log(computerChoice);
+    return determineOutcome(humanChoice, computerChoice);
+}
+
+console.log(playRound());
